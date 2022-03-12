100ce803d206d0477a542e5e9750859b5e2a8f85
ye.yanbin <ye.yanbin@lifecycle.cn>
2022-03-12 22:31:55 +0800

    项目初始化

---
+++ nio-web/src/test/java/com/itan/nioweb/NioWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itan.nioweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NioWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
